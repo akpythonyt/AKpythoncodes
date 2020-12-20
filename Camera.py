ed0a148cd40ce53b502e5c9070177c1e7e378f91
AKpython <72851630+akpythonyt@users.noreply.github.com>
2020-12-20 12:41:26 +0530

    Create your own camera application using python

---
+++ Camera.py
@@ -0,0 +1,56 @@
+import pyautogui
+import PySimpleGUI as sg
+import cv2
+import numpy as np
+
+"""
+Demo program that displays a webcam using OpenCV
+"""
+
+
+def main():
+
+    sg.theme('Black')
+
+    # define the window layout
+    layout = [[sg.Text('OpenCV Demo', size=(40, 1), justification='center', font='Helvetica 20')],
+              [sg.Image(filename='', key='image')],
+              [sg.Button('Record', size=(10, 1), font='Arial 14'),
+               sg.Button('Stop', size=(10, 1), font='Arial 14'),
+               sg.Button('Exit', size=(10, 1), font='Arial 14'),
+               sg.Button('Screenshot',size=(10,1),font='Arial 14') ]]
+
+    # create the window and show it without the plot
+    window = sg.Window('Demo Application - OpenCV Integration',
+                       layout, location=(800, 400))
+
+    # ---===--- Event LOOP Read and display frames, operate the GUI --- #
+    cap = cv2.VideoCapture(0)
+    recording = False
+
+    while True:
+        event, values = window.read(timeout=20)
+        if event == 'Exit' or event == sg.WIN_CLOSED:
+            return
+
+        elif event == 'Record':
+            recording = True
+        elif event=='Screenshot':
+            myScreenshot = pyautogui.screenshot()
+            myScreenshot.save(r'shot.png')
+
+        elif event == 'Stop':
+            recording = False
+            img = np.full((480, 640), 255)
+            # this is faster, shorter and needs less includes
+            imgbytes = cv2.imencode('.png', img)[1].tobytes()
+            window['image'].update(data=imgbytes)
+
+        if recording:
+            ret, frame = cap.read()
+            imgbytes = cv2.imencode('.png', frame)[1].tobytes()  # ditto
+            window['image'].update(data=imgbytes)
+        
+
+
+main()
